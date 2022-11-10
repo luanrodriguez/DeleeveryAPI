b06f81a8b994585b8795867f7b4c7ced9b08a83c
luan <luanrodriguesdev@gmail.com>
2022-11-10 16:44:52 -0300

    adicionado cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import {} from "dotenv/config";
+import cors from 'cors'
 import express from 'express'
 import DeliveryRoute from './routes/DeliveryRoute.js'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(DeliveryRoute)
 
